ced7ad9b6cfa36027d61ed4487ed0ae65da4ad58
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-08-03 13:51:59 +0400

    Added more custom styling and made app grid responsive

--- src/components/App.js
+++ src/components/App.js
@@ -31,10 +31,18 @@ class App extends React.Component {
       <div className="ui container">
         <h1 className="ui violet header center aligned">veedeo.</h1>
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList 
-        onVideoSelect={this.onVideoSelect} 
-        videos={this.state.videos} />
+        <div className="ui stackable grid">
+          <div className="ui row">
+              <div className="eleven wide column">
+                  <VideoDetail video={this.state.selectedVideo} />
+              </div>
+              <div className="five wide column">
+                  <VideoList 
+                    onVideoSelect={this.onVideoSelect} 
+                    videos={this.state.videos} />
+              </div>
+          </div>
+        </div>
       </div>
     )
   }
