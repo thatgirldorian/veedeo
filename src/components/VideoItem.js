0578f36a1cdd4652d29a055ed932fa2443a51997
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-08-03 12:08:14 +0400

    Styled the video detail component & added some styling

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -7,7 +7,7 @@ const VideoItem = ({ video, onVideoSelect }) => {
             <img className="ui image" src={video.snippet.thumbnails.medium.url} />
             <div className="content">
             <a className="header" >{video.snippet.title}</a>
-            <div className="description">{video.snippet.description}</div>
+            <div className="description">{video.snippet.channelTitle}</div>
             </div>
         </div>
     )
